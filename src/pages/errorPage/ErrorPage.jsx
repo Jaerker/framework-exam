e9fd54b40b9c801d8c8c1c3429d16e67733b087e
Jaerker <johanjakberger@gmail.com>
2024-05-07 09:29:46 +0200

    Small bugs fixed

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -10,9 +10,9 @@ const ErrorPage = () => {
         <>
             <h1 className='page-header'>The party was not found!</h1>
             <section className='error-page'>
-                <p><i>Well, this is awkward...</i></p>
-                <p>Vi ber om ursäkt, men det verkar som att du har hamnat på fel fest! Vi rekommenderar att du dubbelkollar din adress och försöker igen.</p>
-                <p> Om problemet kvarstår, kontakta vår support så hjälper vi gärna till att lösa det.</p>
+                <p className='error-paragraph'><i>Well, this is awkward...</i></p>
+                <p className='error-paragraph'>Vi ber om ursäkt, men det verkar som att du har hamnat på fel fest! Vi rekommenderar att du dubbelkollar din adress och försöker igen.</p>
+                <p className='error-paragraph'> Om problemet kvarstår, kontakta vår support så hjälper vi gärna till att lösa det.</p>
                 <Button text='Gå tillbaka' onClick={() => navigate(-1)} />
             </section>
         </>
